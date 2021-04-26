58294ef1bf0a8d0ac706f2279c24a1f0b690f4f1
wkg <wkaige@qq.com>
2021-04-27 01:43:09 +0800

    修改应用标题

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Adb 工具',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
